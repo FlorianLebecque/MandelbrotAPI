9b984e9d652facca99fb24b23e0b81874980d16d
Florian <Florianlebecque@hotmail.com>
2022-11-10 17:15:44 +0100

    Change to step and change default value

--- MandelbrotAPI/Controllers/MandelbrotController.cs
+++ MandelbrotAPI/Controllers/MandelbrotController.cs
@@ -12,9 +12,9 @@ namespace MandelbrotAPI.Controllers {
     [Route("[controller]")]
     public class MandelbrotController : ControllerBase {
 
-
-        [HttpGet(Name ="MandelBrot?from={param1}&to={param2}&res={param3}&iter={param4}")]
-        public MandelbrotSet Get(string from = "-10:-10", string to = "10:10", double res = 1,int iter = 30) {
+        
+        [HttpGet(Name ="MandelBrot?from={param1}&to={param2}&step={param3}&iter={param4}")]
+        public MandelbrotSet Get(string from = "-3:-2", string to = "2:2", double step = 500,int iter = 30) {
 
 
             string[] from_arg = from.Split(':');
@@ -23,9 +23,10 @@ namespace MandelbrotAPI.Controllers {
             Complex v2_from = new Complex(Convert.ToDouble(from_arg[0]), Convert.ToDouble(from_arg[1]));
             Complex v2_to   = new Complex(Convert.ToDouble(  to_arg[0]), Convert.ToDouble(  to_arg[1]));
 
-            Response.Headers.Add("Access-Control-Allow-Origin", "*");
-            return new MandelbrotSet(v2_from, v2_to, res,iter);
+            return new MandelbrotSet(v2_from, v2_to, step,iter);
         }
+        
+        
 
 
     }
