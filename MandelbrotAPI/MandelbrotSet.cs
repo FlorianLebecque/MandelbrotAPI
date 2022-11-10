275064cbe5133cfa3a14e05374842056ad303fd8
Florian <Florianlebecque@hotmail.com>
2022-11-10 17:15:07 +0100

    Change resolution to steps, add img support

--- MandelbrotAPI/MandelbrotSet.cs
+++ MandelbrotAPI/MandelbrotSet.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Server.IIS.Core;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Numerics;
 
 namespace MandelbrotAPI {
@@ -6,20 +8,24 @@ namespace MandelbrotAPI {
         private Complex from { get; set; }
         private Complex to { get; set; }
 
-        private double resolution { get; set; }
+        private double step { get; set; }
 
         private int max_iter { get; set; }
+
+        private double ratio { get; set; }
 
         public List<List<int>> points { get; set; }
             
 
 
-        public MandelbrotSet(Complex from, Complex to, double res,int iter) {
+        public MandelbrotSet(Complex from, Complex to, double step,int iter) {
 
             this.from   = from;
             this.to     = to;
-            this.resolution = res;
+            this.step = step;
             this.max_iter   = iter;
+
+            this.ratio = (this.to.Imaginary - this.from.Imaginary) / (this.to.Real - this.from.Real);
 
             points = this.compute();
         }
@@ -31,9 +37,13 @@ namespace MandelbrotAPI {
 
             List<List<int>> points_ = new List<List<int>>();
 
-            for(double r = from.Real; r < to.Real; r+= this.resolution) {
+            double r_inc = (to.Real - from.Real) / this.step;
+            double i_inc = (to.Imaginary - from.Imaginary) / this.step;
+
+
+            for (double r = from.Real; r < to.Real; r+= r_inc) {
                 List<int> temp = new List<int>();
-                for(double i = from.Imaginary; i < to.Imaginary; i += this.resolution) {
+                for(double i = from.Imaginary; i < to.Imaginary; i += i_inc) {
 
                     c = new Complex(r,i);
 
@@ -58,5 +68,42 @@ namespace MandelbrotAPI {
 
         }
 
+        public byte[]? SaveImg() {
+
+            //create image from list of points
+
+            // Create 2D array of integers
+            int width = points.Count();
+            int height = points[0].Count();
+            int stride = width * 4;
+            int[,] integers = new int[width, height];
+
+           
+            for (int x = 0; x < points.Count(); x++) {
+                for (int y = 0; y < points[x].Count(); y++) {
+
+                    byte colors = (byte)(255 * points[x][y]);
+
+                    byte[] bgra = new byte[] { colors, colors, colors, 255 };
+                    
+                    integers[x, y] = BitConverter.ToInt32(bgra, 0);
+                }
+            }
+
+            
+
+            // Copy into bitmap
+            Bitmap bitmap;
+            unsafe {
+                fixed (int* intPtr = &integers[0, 0]) {
+                    bitmap = new Bitmap(width, (int)(width * this.ratio), stride, PixelFormat.Format32bppRgb, new IntPtr(intPtr));
+                }
+            }
+
+
+            ImageConverter converter = new ImageConverter();
+            return converter.ConvertTo(bitmap, typeof(byte[])) as byte[];
+
+        }
     }
 }
