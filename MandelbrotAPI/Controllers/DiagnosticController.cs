68a5c0b3bf81b495f62d0a9a2551b89fac426419
Florian <Florianlebecque@hotmail.com>
2022-11-16 07:53:54 +0100

    Create a diagnosis controller

---
+++ MandelbrotAPI/Controllers/DiagnosticController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MandelbrotAPI.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiagnosticController : ControllerBase {
+
+
+
+        [HttpGet(Name = "Diagnosis")]
+        public float Get() {
+
+            PerformanceCounter a = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            a.NextValue();
+
+            Thread.Sleep(1000);
+
+            return a.NextValue();
+        }
+    }
+}
