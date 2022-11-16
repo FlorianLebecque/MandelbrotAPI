ca70111da537ffcce7113cd4b7d4733df677818e
Florian <Florianlebecque@hotmail.com>
2022-11-16 07:54:41 +0100

    Create a queue object

--- MandelbrotAPI/QueueCall.cs
+++ MandelbrotAPI/QueueCall.cs
@@ -1,7 +1,23 @@
-﻿namespace MandelbrotAPI {
+﻿using System.Numerics;
+
+namespace MandelbrotAPI {
     public class QueueCall {
-        public DateTime time { get; set; }
 
+        private int off_i, off_j, iter;
+        private Complex from, to;
+        private double step;
 
+        public QueueCall(int off_i, int off_j, Complex from, Complex to, double step, int iter) {
+            this.off_i = off_i;
+            this.off_j = off_j;
+            this.from = from;
+            this.to = to;
+            this.step = step;
+            this.iter = iter;
+        }
+
+        public MandelBrotPart Compute() {
+            return new MandelBrotPart(off_i, off_j, from, to, step, iter);
+        }
     }
 }
